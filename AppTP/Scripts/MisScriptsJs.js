9cbf03858ff416d04618ca849d57be47aaf85258
diegoazh <diegoazh2003@gmail.com>
2016-06-19 15:36:05 -0300

    Menu pegajoso que quería pablo Pablo.

--- AppTP/Scripts/MisScriptsJs.js
+++ AppTP/Scripts/MisScriptsJs.js
@@ -1,4 +1,14 @@
 ﻿$(document).ready(function () {
+    var altura = $('#main_menu').offset().top;
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > altura) {
+            $('#main_menu').addClass('navbar-fixed-top');
+        } else {
+            $('#main_menu').removeClass('navbar-fixed-top');
+        }
+    });
+
     $("a").click(function () {
         $('#login_form').modal("show");
     });
